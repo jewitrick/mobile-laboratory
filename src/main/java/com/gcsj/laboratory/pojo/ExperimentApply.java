0df758f0d470669a8c204b8cbb50d550b7470dc7
zhongyuqun <1104339775@qq.com>
2020-12-03 21:04:04 +0800

    fix: 修复了同个学校的负责人可以重复申请同一个实验的bug

--- src/main/java/com/gcsj/laboratory/pojo/ExperimentApply.java
+++ src/main/java/com/gcsj/laboratory/pojo/ExperimentApply.java
@@ -1,6 +1,9 @@
 package com.gcsj.laboratory.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import tk.mybatis.mapper.annotation.KeySql;
 
 import javax.persistence.Id;
@@ -9,6 +12,9 @@ import javax.persistence.Transient;
 import java.util.Date;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "experi_apply")
 public class ExperimentApply {
     @Id
--- src/main/java/com/gcsj/laboratory/service/ExperimentApplyService.java
+++ src/main/java/com/gcsj/laboratory/service/ExperimentApplyService.java
@@ -18,6 +18,7 @@ import org.springframework.util.ObjectUtils;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class ExperimentApplyService {
@@ -41,6 +42,11 @@ public class ExperimentApplyService {
     }
 
     public CommonResponse<ExperimentApply> insert(long id, ExperimentApply experimentApply) {
+
+        if (!checkIfRepeated(id, experimentApply.getExperi_id())) {
+            return new CommonResponse<>(false, "申请失败！不能重复申请该实验！", null);
+        }
+
         experimentApply.setUser_id(id);
         experimentApply.setState(0);
         experimentApply.setResult("请等待教育局审核");
@@ -49,6 +55,42 @@ public class ExperimentApplyService {
             return new CommonResponse<>(true, "申请成功", null);
         }
         return new CommonResponse<>(false, "申请失败", null);
+    }
+
+    /**
+     * 判断是否重复申请实验
+     *
+     * @param id       申请实验者id
+     * @param experiId 被申请的实验id
+     * @return true 没有重复申请，false 重复申请
+     */
+    private boolean checkIfRepeated(long id, long experiId) {
+
+        List<ExperimentApply> experimentApplyList = this.experimentApplyMapper.select(
+                ExperimentApply.builder().experi_id(experiId).build());
+
+        boolean flag = true;
+
+        for (ExperimentApply foundExperimentApply : experimentApplyList) {
+
+            Long userId = foundExperimentApply.getUser_id();
+
+            // 该条已有的实验申请记录对应的学校id
+            Long foundSchoolId = this.userSchoolMapper.selectOne(UserSchool.builder()
+                    .user_id(userId).build()).getSchool_id();
+
+            // 新申请者的学校id
+            Long newSchoolId = this.userSchoolMapper.selectOne(UserSchool.builder()
+                    .user_id(id).build()).getSchool_id();
+
+            if ((Objects.equals(id, userId) || Objects.equals(foundSchoolId, newSchoolId))
+                    && !Objects.equals(foundExperimentApply.getState(), 2)) {
+                flag = false;
+            }
+
+        }
+
+        return flag;
     }
 
     public QueryResponse<ExperimentApply> findPageByUserId(Long id, int currentPage, int pageSize, Long state) {
@@ -88,10 +130,10 @@ public class ExperimentApplyService {
         experimentApply.setState(state);
         experimentApply.setResult("请等待教育局申请用车");
         int i = this.experimentApplyMapper.updateByPrimaryKey(experimentApply);
-        if (i==1){
-            return new CommonResponse<>(true,"您已成功提交审核",null);
+        if (i == 1) {
+            return new CommonResponse<>(true, "您已成功提交审核", null);
         }
-        return new CommonResponse<>(false,"审核失败",null);
+        return new CommonResponse<>(false, "审核失败", null);
 
     }
 
@@ -100,9 +142,9 @@ public class ExperimentApplyService {
         experimentApply.setState(state);
         experimentApply.setResult(result);
         int i = this.experimentApplyMapper.updateByPrimaryKey(experimentApply);
-        if (i==1){
-            return new CommonResponse<>(true,"您已驳回实验",null);
+        if (i == 1) {
+            return new CommonResponse<>(true, "您已驳回实验", null);
         }
-        return new CommonResponse<>(false,"驳回失败",null);
+        return new CommonResponse<>(false, "驳回失败", null);
     }
 }
